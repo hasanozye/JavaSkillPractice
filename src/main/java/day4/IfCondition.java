d367f844109c16908f2bc044f24a6daa5c78c120
Hasan <hasanozyer06@gmail.com>
2025-12-06 13:58:02 +0300

    Add day4 package with examples of control statements in Java.

---
+++ src/main/java/day4/IfCondition.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class IfCondition {
+
+    public static void main(String[] args){
+
+        int personAge = 20;
+
+        if(personAge >= 18){
+            System.out.println("Eligible for vote");
+        }
+    }
+
+}
---
+++ src/main/java/day4/IfElseCondition.java
@@ -0,0 +1,15 @@
+package day4;
+
+public class IfElseCondition {
+    public static void main(String[] args) {
+        int person_age = 10;
+
+        if (person_age >= 18) {
+            System  .out.println("Eligible for vote");
+        } else {
+            System.out.println("Not Eligible for vote");
+        }
+
+    }
+
+}
